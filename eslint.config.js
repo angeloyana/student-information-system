ebe02eceefb94726ca7c164df4a7c71f2378d97b
Angelo Yana <angelocyana@gmail.com>
2025-01-14 12:47:05 +0800

    chore: setup eslint

---
+++ eslint.config.js
@@ -0,0 +1,32 @@
+import prettier from 'eslint-config-prettier';
+import js from '@eslint/js';
+import { includeIgnoreFile } from '@eslint/compat';
+import svelte from 'eslint-plugin-svelte';
+import svelteParser from 'svelte-eslint-parser';
+import globals from 'globals';
+import { fileURLToPath } from 'node:url';
+
+const gitignorePath = fileURLToPath(new URL('./.gitignore', import.meta.url));
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  includeIgnoreFile(gitignorePath),
+  js.configs.recommended,
+  ...svelte.configs['flat/recommended'],
+  prettier,
+  ...svelte.configs['flat/prettier'],
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+  },
+  {
+    files: ['**/*.svelte.js'],
+    languageOptions: {
+      parser: svelteParser,
+    },
+  },
+];
